726781de1c31512842bce24a10332a9345af9d44
Ben <ben.williams@noaa.gov>
2026-03-31 13:29:04 -0800

    smoothed

--- R/selectivity.R
+++ R/selectivity.R
@@ -15,9 +15,10 @@ to_one <- function(x) {
 #' @param adj based upon index that starts at 1 - unless adjusted to start at recruitment age
 #' doesn't really do anything other than scale
 #' @export
-sel_logistic <- function(age, log_a50, delta, adj) {
+sel_logistic <- function(age, log_a50, delta, adj=0) {
   a50 = exp(log_a50)
-  sel = 1 / (1 + exp(-log(19) * ((age + adj) - a50) / delta))
+  x = age + adj
+  sel = 1 / (1 + exp(-log(19) * (x - a50) / delta))
   # sel = sel / max(sel)
   sel
 }
@@ -30,10 +31,11 @@ sel_logistic <- function(age, log_a50, delta, adj) {
 #' @param adj based upon index that starts at 1 - unless adjusted to start at recruitment age
 #' doesn't really do anything other than scale
 #' @export
-sel_gamma <- function(age, b50, delta, adj) {
+sel_gamma <- function(age, log_b50, delta, adj=0) {
   b50 = exp(log_b50)
+  x = age + adj
   denom = 0.5 * (sqrt(b50^2 + 4 * delta^2) - b50)
-  sel = (((age + adj) / b50)^(b50 / denom)) * exp((b50 - (age + adj)) / denom)
+  sel = ((x / b50)^(b50 / denom)) * exp((b50 - x) / denom)
   # sel = sel / max(sel)
   sel
 }
@@ -41,23 +43,40 @@ sel_gamma <- function(age, b50, delta, adj) {
 #' @export
 sel_double_normal <- function(age, log_a50, delta, delta2, adj = 0) {
   a50 = exp(log_a50)
-  x_val = age + adj - a50
+  x = age + adj - a50
   # delta for the ascending limb (left) and delta2 for the descending limb (right)
-  sel <- ifelse(x_val <= 0,
-                exp(-(x_val^2) / (2 * delta^2)),
-                exp(-(x_val^2) / (2 * delta2^2)))
+  sel <- ifelse(x <= 0,
+                exp(-(x^2) / (2 * delta^2)),
+                exp(-(x^2) / (2 * delta2^2)))
   # sel = sel / max(sel)
   sel
 }
 
 #' @export
-sel_double_logistic <- function(age, log_a50_a, k_asc, log_a50_d, k_desc, adj = 0) {
+sel_double_logistic <- function(age, log_a50_a, delta, log_a50_d, log_delta2, adj = 0) {
   a50_a = exp(log_a50_a)
   a50_d = exp(log_a50_d)
+  delta2 = exp(log_delta2)
+  x = age + adj
   # ascending limb
-  asc = 1 / (1 + exp(-k_asc * (age + adj - a50_a)))
+  asc = 1 / (1 + exp(-delta * (x - a50_a)))
   # escending limb
-  desc = 1 / (1 + exp(-k_desc * (age + adj - a50_d)))
+  desc = 1 / (1 + exp(-delta2 * (x - a50_d)))
+  sel = asc * desc
+  # sel = sel / max(sel)
+  sel
+}
+
+#' @export
+sel_double_logistic_smooth <- function(age, log_a50_a, delta, log_a50_d, log_delta2, adj = 0) {
+  a50_a = exp(log_a50_a)
+  a50_d = a50 + exp(log_a50_d)
+  delta2 = exp(log_delta2)
+  x = age + adj
+  # ascending limb
+  asc = 1 / (1 + exp(-delta * (x - a50_a)))
+  # escending limb
+  desc = 1 / (1 + exp(-delta2 * (x - a50_d)))
   sel = asc * desc
   # sel = sel / max(sel)
   sel
@@ -79,8 +98,12 @@ get_slx <- function(ages, type, pars, adj) {
       sel_double_normal(ages, pars[1], pars[2], pars[3], adj)
     },
     "4" = {
-      # double logistic: pars[1] = a50_asc, pars[2] = k_asc, pars[3] = a50_desc, pars[4] = k_desc
+      # double logistic: pars[1] = log_a50_a, pars[2] = delta, pars[3] = log_a50_d, pars[4] = log_delta2
       sel_double_logistic(ages, pars[1], pars[2], pars[3], pars[4], adj)
+    },
+    "5" = {
+      # double logistic smoothed: pars[1] = log_a50_a, pars[2] = delta, pars[3] = log_a50_d, pars[4] = log_delta2
+      sel_double_logistic_smooth(ages, pars[1], pars[2], pars[3], pars[4], adj)
     }
   )
 }
